1a9c9150ae17e033b6b46a4da9a70981fa71bfde
sebagb <sgbcmpu@gmail.com>
2025-10-14 11:22:43 +0200

    Correct order price type to decimal

--- OrderService.Application/Models/Order.cs
+++ OrderService.Application/Models/Order.cs
@@ -6,6 +6,6 @@ public class Order
     public required Guid CustomerId { get; set; }
     public required Guid ProductId { get; set; }
     public required DateOnly OrderedOn { get; set; }
-    public required int Price { get; set; }
+    public required decimal Price { get; set; }
     public required int Quantity { get; set; }
 }
