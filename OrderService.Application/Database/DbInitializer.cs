ebeccf393731f923ef0160d9a246caa3b84254d8
sebagb <sgbcmpu@gmail.com>
2025-11-05 12:19:56 +0100

    Check if database is initialized and keep trying if connection is unnreachable

--- OrderService.Application/Database/DbInitializer.cs
+++ OrderService.Application/Database/DbInitializer.cs
@@ -1,3 +1,4 @@
+using System.Data.Common;
 using Dapper;
 
 namespace OrderService.Application.Database;
@@ -8,18 +9,32 @@ public class DbInitializer(IDbConnectionFactory connectionFactory)
 
     public void Initialize()
     {
-        using var connection = connectionFactory.CreateConnection();
-
-        connection.Execute("""
-        CREATE TABLE IF NOT EXISTS Orders (
-            OrderId CHAR(36) PRIMARY KEY,
-            CustomerId CHAR(36),
-            ProductId CHAR(36),
-            OrderedOn DATETIME,
-            Price DECIMAL(7,2),
-            Quantity INTEGER,
-            Status VARCHAR(100)
-        )
-        """);
+        try
+        {
+            using var connection = connectionFactory.CreateConnection();
+            connection.Execute("""
+                CREATE TABLE IF NOT EXISTS Orders (
+                    OrderId CHAR(36) PRIMARY KEY,
+                    CustomerId CHAR(36),
+                    ProductId CHAR(36),
+                    OrderedOn DATETIME,
+                    Price DECIMAL(7,2),
+                    Quantity INTEGER,
+                    Status VARCHAR(100)
+                )
+                """);
+        }
+        catch (DbException ex)
+        {
+            var msg =
+                $"DbInitializer exception. {ex.Message}. Attempting again ...";
+            Console.WriteLine(msg);
+            Thread.Sleep(5000);
+            Initialize();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
     }
 }
